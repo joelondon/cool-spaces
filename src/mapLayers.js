6e6fe8d0e11ed726115156e578e00657dce0d569
joelondon <joseph.leach@london.gov.uk>
2021-05-12 00:14:20 +0100

    initial commit

---
+++ src/mapLayers.js
@@ -0,0 +1,30 @@
+export const mapLayers = {
+  'borough designated cool spaces': {
+    value: 'boroughdesignatedcoolspaces',
+    showing: true,
+    about: 'boroughdesignatedcoolspaces'
+  },
+
+  'other public shaded green areas': {
+    value: 'otherpublicshadedgreenareas',
+    showing: true,
+    about: 'otherpublicshadedgreenareas'
+  },
+  'water fountains': {
+    value: 'waterfountains',
+    showing: true,
+    about: 'waterfountains'
+  },
+
+  'water courses': {
+    value: 'watercourses',
+    showing: true,
+    about: 'watercourses'
+  },
+
+  'density of cooler land surface': {
+    value: 'avgLST_London_UrbanAtlas',
+    showing: true,
+    about: 'avgLST_London_UrbanAtlas'
+  }
+}
