2a43f5e28320aa20ca9eab03dcfb72cd92a50e4a
joelondon <joseph.leach@london.gov.uk>
2021-06-16 11:58:51 +0100

    update about text

--- src/mapAbout.js
+++ src/mapAbout.js
@@ -6,13 +6,13 @@ const markdown = `
 
 Cool spaces are indoor or outdoor areas where Londoners can take respite on hot days. Cool space sites have been put forward by boroughs, community groups, faith based and other organisations following an  invitation to register process. The cool spaces are categorised by three tiers that can apply to indoor and outdoor spaces:
 
-* Tier 1 has the greatest number of minimum amenities that Londoners can expect from a cool space.
-* Tier 2 has the second greatest number of minimum amenities.
-* Tier 3 includes outdoor green and blue spaces which, although not providing the minimum amenities for tiers 1 and 2, provide suitable shading conditions.
+* Tier 1 spaces have the greatest number of amenities that Londoners can expect from a cool space including access to toilets, free drinking water and full access to disabled persons.
+* Tier 2 spaces offer some of the amenities above, but not all. Tier 2 cool spaces still provide good cooling conditions and have the level of accessibility to disabled people clearly stated.
+* Tier 3 includes outdoor green and blue spaces which, although not offering the minimum amenities for tiers 1 and 2, provide suitable cooling conditions.
 
 The tiered approach to classifying cool spaces is meant to provide Londoners with a better understanding of what can be expected from spaces and how certain spaces can better fit their needs. More information about cool spaces and the criteria for the different tiers can be found here.
 
-When using London’s parks and green spaces, please remember to follow guidelines on social distancing. Read PHE guidance on Coping with heat and COVID-19 and the Mayor’s guidance on keeping cool on hot days.
+When using London’s parks and green spaces, please remember to [follow guidelines on social distancing](https://www.london.gov.uk/coronavirus/social-distancing-guidance/london-parks-and-green-spaces-covid-19-guidance). Read PHE guidance on [Coping with heat and COVID-19](https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/980924/Beat-the-Heat_Leaflet_COVID-19_2021.pdf) and the Mayor’s guidance on [keeping cool on hot days](https://www.london.gov.uk/what-we-do/environment/coping-hot-weather-london).
 
 Data information and credits:
 
