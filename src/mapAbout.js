80176ee2bf7d6f3b87b71c04c3a31bd1ffaade04
joelondon <joseph.leach@london.gov.uk>
2021-06-16 12:17:45 +0100

    add explanatory text

--- src/mapAbout.js
+++ src/mapAbout.js
@@ -12,6 +12,8 @@ Cool spaces are indoor or outdoor areas where Londoners can take respite on hot 
 
 The tiered approach to classifying cool spaces is meant to provide Londoners with a better understanding of what can be expected from spaces and how certain spaces can better fit their needs. More information about cool spaces and the criteria for the different tiers can be found here.
 
+The tree canopy layer intends to provide indicative information on potential shade in public open areas across London. These areas should not be confused with designated cool spaces and are not always suitable alternatives for them.
+
 When using London’s parks and green spaces, please remember to [follow guidelines on social distancing](https://www.london.gov.uk/coronavirus/social-distancing-guidance/london-parks-and-green-spaces-covid-19-guidance). Read PHE guidance on [Coping with heat and COVID-19](https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/980924/Beat-the-Heat_Leaflet_COVID-19_2021.pdf) and the Mayor’s guidance on [keeping cool on hot days](https://www.london.gov.uk/what-we-do/environment/coping-hot-weather-london).
 
 Data information and credits:
