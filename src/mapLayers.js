78b171ab13561eff7eb672d32a8d5ddb188bcc25
joelondon <joseph.leach@london.gov.uk>
2021-06-16 12:01:59 +0100

    add full stops

--- src/mapLayers.js
+++ src/mapLayers.js
@@ -3,27 +3,27 @@ export const mapLayers = {
     value: 'boroughdesignatedcoolspaces',
     showing: true,
     about:
-      'These locations have been designated as cool spaces where refuge can be sought during heatwaves'
+      'These locations have been designated as cool spaces where refuge can be sought during heatwaves.'
   },
   // 'Water fountains': {
   //   value: 'findafountain',
   //   showing: true,
-  //   about: 'Source: find a fountain'
+  //   about: 'Source: find a fountain.'
   // },
   'Water courses': {
     value: 'watercourses',
     showing: true,
-    about: 'Water cools the air, so these places can be less hot'
+    about: 'Water cools the air, so these places can be less hot.'
   },
   'Tree canopy cover': {
     value: 'treecanopy',
     showing: true,
-    about: 'Indicative location of areas with shading from trees'
+    about: 'Indicative location of areas with shading from trees.'
   },
   'Cooler areas': {
     value: 'avgLST_London_UrbanAtlas',
     showing: true,
     about:
-      "Modelled average land surface temperature - the more visible this layer's information is, the cooler the area is likely to be"
+      "Modelled average land surface temperature - the more visible this layer's information is, the cooler the area is likely to be."
   }
 }
